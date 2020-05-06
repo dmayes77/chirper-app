c10f5d45093f8cef408987ef3b4fec673ea44351
dmayes77 <dmayes77@gmail.com>
2020-05-06 11:19:29 -0400

    first commit

---
+++ src/actions/shared.js
@@ -0,0 +1,15 @@
+import { getInitialData } from '../utils/api';
+import { setAuthedUser } from './authedUser';
+import { receiveTweets } from './tweets';
+import { receiveUsers } from './users';
+
+const AUTHED_ID = 'tylermcginnis';
+
+const handleInitialData = () => dispatch =>
+	getInitialData().then(({ users, tweets }) => {
+		dispatch(receiveUsers(users));
+		dispatch(receiveTweets(tweets));
+		dispatch(setAuthedUser(AUTHED_ID));
+	});
+
+export { handleInitialData };
